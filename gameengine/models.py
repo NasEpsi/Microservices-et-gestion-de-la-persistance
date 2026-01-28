a95a59ec44e5c23c8aa20bce0efef58d75cce91b
NasEpsi <nassimouachen@yahoo.com>
2026-01-28 12:16:13 +0100

    Ready for tommorow

--- gameengine/models.py
+++ gameengine/models.py
@@ -15,7 +15,17 @@ class PlayersPlayGames(Base):
     game_id: Mapped[int] = mapped_column(ForeignKey("game.id"), primary_key=True)
     player: Mapped["Player"] = relationship(back_populates="games")
     game: Mapped["Game"] = relationship(back_populates="players")
-        
+    position_col: Mapped[int] = mapped_column(nullable=False, default=0) 
+    position_row: Mapped[int] = mapped_column(nullable=False, default=0)
+    next_move_col : Mapped[int] = mapped_column()
+    next_move_row : Mapped[int] = mapped_column()
+    
+    def __repr__(self):
+        return json.dumps({
+            "player": self.player,
+            "game": self.game
+        })
+
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.player == other.player and self.game == other.game
@@ -28,7 +38,11 @@ class Game(Base):
     title: Mapped[str] = mapped_column(unique=True, nullable=False)
     started: Mapped[bool] = mapped_column(nullable=False, default=False)
     players: Mapped[List["PlayersPlayGames"]] = relationship(back_populates="game")
-
+    active_turn: Mapped[int] = mapped_column(nullable=False, default=0)
+    max_turn: Mapped[int] = mapped_column(nullable=False, default=0)
+    nb_rows: Mapped[int] = mapped_column(nullable=False, default=0)
+    nb_cols: Mapped[int] = mapped_column(nullable=False, default=0)
+    
     def __repr__(self):
         return self.title
     
